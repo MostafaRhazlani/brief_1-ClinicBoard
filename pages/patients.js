8dc0f2a89089d81ffe5a0aad88c5bb2cc2ed48ed
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2025-09-21 16:35:26 +0100

    CBD-12 - Add patient creation form to add new patient

--- pages/patients.js
+++ pages/patients.js
@@ -1,11 +1,14 @@
+import Form from '../components/form-modal.js';
+import Modal from '../components/popup-form-modal.js';
+import { setData, getData } from '../localStorage.js';
+
 export default function Patients() {
     const container = document.createElement('div');
     container.className = 'content';
     container.innerHTML = `
-
         <div class="patient-title">
             <h2>Patients</h2>
-            <button class="btn">
+            <button class="btn add-patient-btn">
                 <i class="fa-solid fa-user-plus"></i>
                 Add Patient
             </button>
@@ -91,5 +94,53 @@ export default function Patients() {
             </div>
         </div>
     `
+
+    // Create the form for patient creation
+    const form = Form({
+        fields: [
+            { type: "text", id: "patient_name", label: "Patient Name", placeholder: "Enter patient name" },
+            { type: "email", id: "patient_email", label: "Email", placeholder: "Enter patient email" },
+            { type: "text", id: "patient_phone", label: "Phone Number", placeholder: "Enter phone number" },
+            { type: "submit", value: 'Create Patient', className: 'btn' }
+        ]
+    });
+
+    // Create modal form
+    const modal = Modal({ content: form });
+    container.appendChild(modal);
+
+    container.querySelector('.add-patient-btn').addEventListener('click', () => {
+        modal.open();
+    });
+
+    // Handle submit form to store new patient in localstorage
+    form.addEventListener('submit', (e) => {
+        e.preventDefault();
+        const name = form.querySelector('#patient_name').value.trim();
+        const email = form.querySelector('#patient_email').value.trim();
+        const phone = form.querySelector('#patient_phone').value.trim();
+
+        if (!name || !email || !phone) {
+            alert('All fields are required.');
+            return;
+        }
+
+        let data = getData('clinicApp:data');
+
+        // Add new patient
+        data.patients.push({
+            name,
+            email,
+            phone,
+            createdAt: new Date().toISOString()
+        });
+
+        setData('clinicApp:data', data);
+
+        alert('Patient added!');
+        modal.close();
+
+    });
+
     return container;
 }
