e6e4819b6d8c16efed9f0c42d6040ea197bc1e11
Mostafa Rhazlani <mo.rhazlani@gmail.com>
2025-09-21 15:49:18 +0100

    Remove about and home page components

--- pages/about.js
+++
@@ -1,5 +0,0 @@
-export default function about() {
-    const parent = document.createElement('div');
-    parent.innerHTML = '<h1>About Page</h1>'
-    return parent;
-}
--- pages/home.js
+++
@@ -1,5 +0,0 @@
-export default function home() {
-    const parent = document.createElement('div');
-    parent.innerHTML = `<h1>Home Page</h1>`
-    return parent;
-}
